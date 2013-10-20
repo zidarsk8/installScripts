5fa2353a0c8473de4cb185e881128eece573b02f
Miha Zidar <zidarsk8@gmail.com>
2013-10-20 13:04:09 +0200

    refactor in progress

--- ceph/network.sh
+++ ceph/network.sh
@@ -3,24 +3,37 @@
 
 #### prepair files ####
 
-mkdir -p ~/.ssh
-[ -a ~/.ssh/config ] || touch ~/.ssh/config
 
+# add hosts and ssh config entry for given IP and name ####
 
+function add_hosts_ssh_entry {
+    
+    if [ "$#" -lt 1 ]; then
+        echo "usage: add_hosts_ssh_entry ip name ssh_key_file ssh_username [ssh_port]"
+        return 1
+    fi
 
-#### add hosts and ssh config entry for given IP and name ####
+    local ip=$1
+    local name=$2
+    local ssh_key=$3
+    local ssh_username=$4
+    local ssh_port=""
+    [ -n "$5" ] || local ssh_port="Port $5"
 
-function add_node_entry {
-  if ! grep -q $2 /etc/hosts; then
-    echo "$1 $2" >> /etc/hosts
-  fi
-  
-  if ! grep -q $2 ~/.ssh/config; then
-    echo "Host $2
-        Hostname $2
-        IdentityFile ~/.ssh/${SSH_KEY_NAME}
-        User ${CEPH_USERNAME}" >> ~/.ssh/config
-  fi
+    mkdir -p ~/.ssh
+    [ -a ~/.ssh/config ] || touch ~/.ssh/config
+
+    if ! grep -q $name /etc/hosts; then
+        echo "$ip $name" >> /etc/hosts
+    fi
+
+    if ! grep -q $name ~/.ssh/config; then
+        echo "Host $name
+        Hostname $name
+        IdentityFile ~/.ssh/${ssh_key}
+        User ${ssh_username}
+        ${ssh_port}" >> ~/.ssh/config
+    fi
 }
 
 # generate hosts and ssh config file entries for ceph admin and ceph nodes 
@@ -28,31 +41,45 @@ function add_node_entry {
 echo "generating hosts and ssh config entries"
 add_node_entry "${NETWORK_IP_PREFIX}${NETWORK_START_IP}" "ceph_admin"
 for (( CURRENT_NODE=1; CURRENT_NODE<=$NODE_COUNT; CURRENT_NODE++ )); do
-  add_node_entry "${NETWORK_IP_PREFIX}$(($NETWORK_START_IP + $CURRENT_NODE))" "ceph_node_${CURRENT_NODE}" 
+    add_node_entry "${NETWORK_IP_PREFIX}$(($NETWORK_START_IP + $CURRENT_NODE))" "ceph_node_${CURRENT_NODE}" 
 done
 
 
 
 #set up internal network interface
 
-echo "seting up internal network interface"
-if [ -n "$NETWORK_INTERFACE" ]; then 
-  sed -i "s/HOSTNAME=.*/HOSTNAME=${HOSTNAME}/g" /etc/sysconfig/network
+function set_hostname {
+    if [ "$#" -lt 1 ]; then
+        echo "usage: set_hostname hostname"
+        return 1
+    fi
+    sed -i "s/HOSTNAME=.*/HOSTNAME=${HOSTNAME}/g" /etc/sysconfig/network
+}
 
-  echo "DEVICE=${NETWORK_INTERFACE} 
+function config_interface {
+
+    local interface=$1
+    local network=$2
+    local netmask=$3
+    local ip=$4
+    local gateway=$5
+    echo "seting up internal network interface: $interface"
+    if [ -n "$interface" ]; then 
+
+        echo "DEVICE=${interface} 
 BOOTPROTO=none 
 NM_CONTROLLED="no"
 ONBOOT=yes 
 NAME='Ceph internal network'
-NETWORK=${NETWORK_IP_PREFIX}0
-NETMASK=255.255.255.0 
-IPADDR=${NETWORK_IP}
-GATEWAY=${NETWORK_GATEWAY}
-USERCTL=no" > /etc/sysconfig/network-scripts/ifcfg-${NETWORK_INTERFACE}
+NETWORK=${network}0
+NETMASK=${netmask}
+IPADDR=${ip}
+GATEWAY=${gateway}
+USERCTL=no" > /etc/sysconfig/network-scripts/ifcfg-${interface}
 
-  #chkconfig network on
-  /etc/init.d/network restart
+        #chkconfig network on
+        /etc/init.d/network restart
 
-fi
-
+    fi
+}
 
